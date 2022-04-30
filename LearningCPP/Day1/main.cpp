5b63cabecb8afef895d3ce601d16cec3bf30e4ea
Jasper Oh <ohyj0906@gmail.com>
2022-04-30 08:00:01 +0000

    operator

--- LearningCPP/Day1/main.cpp
+++ LearningCPP/Day1/main.cpp
@@ -4,3 +4,15 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+int otherone() {
+    auto result = (10 <= 20 ) > 0;
+    std::cout << result << std::endl;
+}
+
+/* fold-operator:
+ *  <=
+ *  relational 
+ *
+ *
+ * */
