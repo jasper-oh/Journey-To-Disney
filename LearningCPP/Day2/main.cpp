3afc48292c2a7face5788333600980cf206914cf
Jasper Oh <ohyj0906@gmail.com>
2022-05-13 08:00:01 +0000

    first learn about stream and their purposes

--- LearningCPP/Day2/main.cpp
+++ LearningCPP/Day2/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+/*
+ * different type of stream
+ *
+ * 0. std::cout -> Printing data to the console (terminal)
+ * 1. std::cin -> Reading data from the terminal
+ * 2. std::cerr -> Printing errors to the console
+ * 3. std::clog -> printing log messages to the console
+ */
+
 void input_output(){
-    
+
 }
 
 int main() {
