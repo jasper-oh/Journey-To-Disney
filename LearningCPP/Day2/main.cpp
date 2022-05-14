47a395ea4517d8b345852b8f71683f88a0d8c760
Jasper Oh <ohyj0906@gmail.com>
2022-05-14 08:00:01 +0000

    Print data with different message

--- LearningCPP/Day2/main.cpp
+++ LearningCPP/Day2/main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 
 /*
- * different type of stream
+ * Different types of stream
  *
  * 0. std::cout -> Printing data to the console (terminal)
  * 1. std::cin -> Reading data from the terminal
  * 2. std::cerr -> Printing errors to the console
  * 3. std::clog -> printing log messages to the console
+ * 4. std::endl -> why didn't he mention about it?
  */
+
+void use_differnet_stream(){
+    std::cout << "Hello World!" << std::endl;
+    std::cerr << "std::cerr output : Something went wrong" << std::endl;
+    std::clog << "std::clog output : This is a log message" << std::endl;
+
+}
 
 void input_output(){
 
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+
+    use_differnet_stream();
     return 0;
 }
