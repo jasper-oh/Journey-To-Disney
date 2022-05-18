0999abf56476f83c62e981309fc07c6589d85141
Jasper Oh <ohyj0906@gmail.com>
2022-05-18 08:00:01 +0000

    std::getline(std::cin,data) is the new one

--- LearningCPP/Day2/main.cpp
+++ LearningCPP/Day2/main.cpp
@@ -12,7 +12,7 @@
  * Printed sequences are change first, log message appear and then err message appear.
  */
 
-void use_differnet_stream(){
+void use_different_stream(){
     std::cout << "Hello World!" << std::endl;
     std::cerr << "std::cerr output : Something went wrong" << std::endl;
     std::clog << "std::clog output : This is a log message" << std::endl;
@@ -40,12 +40,23 @@ void input_output_2(){
     std::cout << "Hello " << name << "! you are " << age << " years old" << std::endl;
 }
 
+void input_output_3(){
+    int age;
+    std::string full_name;
+    std::cout << "Please type in your full name : " << std::endl;
+    std::getline(std::cin,full_name);
+
+    std::cout << "Type in your age : " << std::endl;
+    std::cin >> age;
+    std::cout << "Hello " << full_name << "! You are " << age << " years old." << std::endl;
+}
 
 int main() {
 //    std::cout << "Hello, World!" << std::endl;
 
-//    use_differnet_stream();
+//    use_different_stream();
     input_output();
+    input_output_2();
     return 0;
 
 }
