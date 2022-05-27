6f3e1a3816940c1ef6e5f19d1e838e483e30550b
Jasper Oh <ohyj0906@gmail.com>
2022-05-27 08:00:01 +0000

    Now I can get understanding the concept of c++

--- LearningCPP/LeetCode/two-sum/main.cpp
+++ LearningCPP/LeetCode/two-sum/main.cpp
@@ -29,11 +29,11 @@ int main() {
     nums.push_back(7);
     nums.push_back(11);
     nums.push_back(15);
-    for (int x : sol.twoSum(nums, 9))
-         cout << x << " ";
+//    for (int x : sol.twoSum(nums, 9))
+//         cout << x << " ";
 
     for ( int i = 0 ; i < sol.twoSum(nums,9).size() ; i++){
-        std::cout << sol.twoSum(nums, 9).emplace_back(i);
+        std::cout << sol.twoSum(nums, 9).emplace_back(i) << " ";
     };
     return 0;
 }
