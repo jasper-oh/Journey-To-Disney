63e4bb02a71f45b7afd77627e3caed859f09633d
Jasper Oh <ohyj0906@gmail.com>
2022-06-08 09:06:09 +0000

    Reviewing yesterday stuff and solve another problem

---
+++ LearningCPP/LeetCode/best-time-to-buy-sell-stock/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices){
+        return 0;
+    }
+
+};
+
+
+int main() {
+    Solution sol;
+    vector<int> prices;
+    int price[] = {7,1,5,3,6,4};
+    for(int x : price){
+        prices.emplace_back(x);
+    };
+
+    for(int x : prices){
+        cout << prices.emplace_back(x) << " ";
+    };
+
+    cout << sol.maxProfit(prices) << " ";
+    return 0;
+}
+
+
