29775427c46cabfdd731954138b568f2a55bf09c
Jasper Oh <ohyj0906@gmail.com>
2022-06-08 09:06:09 +0000

    Make another class but didn't work that I thought

---
+++ LearningCPP/Day2/understanding_scope_resolution_operator.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Jasper Oh on 2022-06-29.
+//
+
+#include "understanding_scope_resolution_operator.h"
+
+
---
+++ LearningCPP/Day2/understanding_scope_resolution_operator.h
@@ -0,0 +1,15 @@
+//
+// Created by Jasper Oh on 2022-06-29.
+//
+
+#ifndef DAY2_UNDERSTANDING_SCOPE_RESOLUTION_OPERATOR_H
+#define DAY2_UNDERSTANDING_SCOPE_RESOLUTION_OPERATOR_H
+
+
+class understanding_scope_resolution_operator {
+public:
+
+};
+
+
+#endif //DAY2_UNDERSTANDING_SCOPE_RESOLUTION_OPERATOR_H
