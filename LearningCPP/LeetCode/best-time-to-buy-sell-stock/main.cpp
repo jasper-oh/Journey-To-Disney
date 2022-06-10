c280d73fe53bb2072bd5b92a0bdf1f381c13a3d8
Jasper Oh <ohyj0906@gmail.com>
2022-06-10 09:06:09 +0000

    understanding vector...

--- LearningCPP/LeetCode/best-time-to-buy-sell-stock/main.cpp
+++ LearningCPP/LeetCode/best-time-to-buy-sell-stock/main.cpp
@@ -16,8 +16,8 @@ int main() {
     Solution sol;
     vector<int> prices {7,1,5,3,6,4};
 
-    for(int x : prices){
-        cout << prices.emplace_back(x) << " ";
+    for(int x = 0 ; x < prices.size() ; x ++){
+        cout << prices[x] << " ";
     };
 
     cout << sol.maxProfit(prices) << " ";
