62280de7f029699635dbb123e80bce1282f07222
Jasper Oh <ohyj0906@gmail.com>
2022-06-12 09:06:09 +0000

    make void method to using as a playground about vector

--- LearningCPP/LeetCode/best-time-to-buy-sell-stock/main.cpp
+++ LearningCPP/LeetCode/best-time-to-buy-sell-stock/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 /*
  * Today's agenda
  * 0. Understanding Vector, and connected method
+ * 1. insert, delete, emplace_back() .. etc..
  * */
 
 class Solution {
@@ -16,16 +17,23 @@ public:
 
 };
 
+void understanding_vector(){
+    vector<int> v = {1,2,3};
+
+}
+
 
 int main() {
-    Solution sol;
-    vector<int> prices {7,1,5,3,6,4};
 
-    for(int x = 0 ; x < prices.size() ; x ++){
-        cout << prices[x] << " ";
-    };
-
-    cout << sol.maxProfit(prices) << " ";
+    
+//    Solution sol;
+//    vector<int> prices {7,1,5,3,6,4};
+//
+//    for(int x = 0 ; x < prices.size() ; x ++){
+//        cout << prices[x] << " ";
+//    };
+//
+//    cout << sol.maxProfit(prices) << " ";
     return 0;
 }
 
