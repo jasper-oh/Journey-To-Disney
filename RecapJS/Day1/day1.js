cdc2408442e45266968e4b71e3187e3b7d94bdd7
Jasper Oh <ohyj0906@gmail.com>
2022-07-08 16:20:13 -0700

    Start Recap JavaScript

---
+++ RecapJS/Day1/day1.js
@@ -0,0 +1,38 @@
+var a = 10
+
+let b = 10
+
+const c = 20
+
+console.log(a*b)
+
+var myVar = 87
+
+myVar++
+
+myVar--
+
+var ourDecimal = 5.7
+
+var myDecimal = 0.0003
+
+var product = 2.0 * 0.0
+
+// Who I guess JS automatically choose the data like integer or float
+
+var quotient
+quotient = 11 / 3
+
+var remainder
+remainder = 11 % 3
+
+var a1 = 3
+
+a1 += 12
+
+console.log(a1)
+
+a1 -= 11
+
+console.log(a1)
+
