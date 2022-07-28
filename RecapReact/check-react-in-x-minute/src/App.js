806afe9f36547a55f84738132c8d6ebf0c0d26fb
Jasper Oh <ohyj0906@gmail.com>
2022-07-28 15:22:40 -0700

    Recap React - organizing todo list

--- RecapReact/check-react-in-x-minute/src/App.js
+++ RecapReact/check-react-in-x-minute/src/App.js
@@ -1,14 +1,26 @@
-import React, {useState} from "react";
+import React, {useState , useRef} from "react";
 import TodoList from "./TodoList";
-
+import {v4 as uuidv4} from "uuid";
 function App() {
-  const [todos, setTodos] = useState(["Todo1" , "Todo2"])
-  //object destructuring (?)
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  //object destructuring (?) -> What is the meaning of object destructing
+
+  function handleAddTodo(e){
+      const name = todoNameRef.current.value
+      if(name === '') return
+      setTodos(prevTodos => {
+        return [...prevTodos, {id :uuidv4() , name:name, complete:false}]
+      })
+      todoNameRef.current.value = null
+      console.log(uuidv4())
+
+  }
   return (
     <>
       <TodoList todos = {todos} />
-      <input type="text"/>
-      <button>Add todo</button>
+      <input ref={todoNameRef} type="text"/>
+      <button onClick={handleAddTodo}>Add todo</button>
       <button>Clear Completed</button>
       <div>0 left todo</div>
     </>
@@ -17,3 +29,4 @@ function App() {
 }
 
 export default App;
+
