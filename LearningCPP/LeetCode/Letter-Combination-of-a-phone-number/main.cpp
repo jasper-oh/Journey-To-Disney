bccb5a40b306f7cf6a4b2dbd74a05b6384ade4cf
Jasper Oh <ohyj0906@gmail.com>
2024-12-03 11:11:35 -0800

    [Add] Letter combination of a phone number

---
+++ LearningCPP/LeetCode/Letter-Combination-of-a-phone-number/main.cpp
@@ -0,0 +1,59 @@
+//
+// Created by Jasper Oh on 2024-11-25.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+
+        if (digits.empty()) {
+            return res;
+        }
+
+        unordered_map<char, string> digitToLetters = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+
+        backtrack(digits, 0, "", res, digitToLetters);
+
+        return res;
+    }
+
+    void backtrack(const string& digits, int idx, string comb, vector<string>& res, const unordered_map<char, string>& digitToLetters) {
+        if (idx == digits.length()) {
+            res.push_back(comb);
+            return;
+        }
+
+        string letters = digitToLetters.at(digits[idx]);
+        for (char letter : letters) {
+            backtrack(digits, idx + 1, comb + letter, res, digitToLetters);
+        }
+    }
+};
+
+
+
+int  main() {
+    Solution* sol;
+    vector<string> answer = sol -> letterCombinations("23");
+
+    for(auto i : answer) {
+        cout << i << endl;
+    }
+
+
+}
